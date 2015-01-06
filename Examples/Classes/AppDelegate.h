0e27e9fe25265319c466f72d85d5b026e1ce0dae
andyque <guanghui8827@126.com>
2015-01-06 14:08:58 +0800

    add mac support

--- Examples/Classes/AppDelegate.h
+++ Examples/Classes/AppDelegate.h
@@ -8,6 +8,8 @@ class  AppDelegate : private cocos2d::Application
 public:
     AppDelegate();
     virtual ~AppDelegate();
+    virtual void initGLContextAttrs();
+
     virtual bool applicationDidFinishLaunching();
     virtual void applicationDidEnterBackground();
     virtual void applicationWillEnterForeground();
---
+++ Examples/Example.ios/Example-Mac/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Example-Mac
+//
+//  Created by guanghui on 1/6/15.
+//  Copyright (c) 2015 CatalystApps. All rights reserved.
+//
+
+#include "../../Classes/AppDelegate.h"
+
+USING_NS_CC;
+
+int main(int argc, char *argv[])
+{
+    AppDelegate app;
+    return Application::getInstance()->run();
+}
